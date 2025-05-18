ac5d973606bd393768455f4a215b1b01b6b7148b
Viktor Pastushenko <viktor.pastushenko@udelphi.com>
2025-05-18 08:40:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write code here
+document.querySelector('thead').addEventListener('click', (e) => {
+  const tHead = e.currentTarget;
+  const tBody = tHead.nextElementSibling;
+  const columnIndex = [...tHead.querySelectorAll('th')].indexOf(e.target);
+
+  const compare = (a, b) => {
+    if (a.innerText < b.innerText) {
+      return -1;
+    }
+
+    if (a.innerText > b.innerText) {
+      return 1;
+    }
+
+    return 0;
+  };
+
+  [...tBody.querySelectorAll('tr')]
+    .map((row, index) => {
+      let innerText = row.querySelectorAll('td')[columnIndex].innerText;
+
+      innerText = innerText.match(/^[$]/)
+        ? parseFloat(innerText.replace(/[.$]/, '').replace(',', '.'))
+        : innerText;
+
+      return {
+        index: index,
+        row: row,
+        innerText: innerText,
+      };
+    })
+    .sort(compare)
+    .forEach((sortedRow) => {
+      tBody.append(sortedRow.row);
+    });
+});
